8d782cb7ef0f68445cbd103e0e95e63ea0a8585b
mkelcik <mkelcik@gmail.com>
2024-02-16 09:39:26 +0100

    Update schema

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-protoc ./proto/*/*.proto --jsonschema_out=./json -I ./proto
+protoc ./proto/*/*.proto --jsonschema_out=./json --jsonschema_opt=file_extension=jsonschema -I ./proto
